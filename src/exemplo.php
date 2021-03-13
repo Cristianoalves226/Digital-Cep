c63a5b236281dcfb55e7e9bf8de085c282367c33
Cristiano <c.alvesdecampos@gmail.com>
2021-03-13 17:53:12 -0400

    Busca inicial no ViaCep

---
+++ src/exemplo.php
@@ -0,0 +1,11 @@
+<?php
+
+use Xcris\DigitalCep\Search;
+
+require_once "vendor/autoload.php";
+
+$busca = new Search;
+
+$resultado = $busca->getAddressFromZipeCode('01001000');
+
+print_r($resultado);
